633ac75e5c9773e022acd41ec6be197cea727322
feikerwu <feikerwu@gmail.com>
2024-12-09 10:01:37 +0800

    feat: 收益计算

--- src/suilend/index.mjs
+++ src/suilend/index.mjs
@@ -1,24 +1,51 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+async function getPoints() {
+  const response = await fetch('https://api.suilend.fi/points/leaderboard');
+  const data = await response.json();
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const rows = data.rows;
 
-// Read and parse the point.json file
-const pointData = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'point.json'), 'utf8')
-);
+  const totalPoints = rows.reduce((sum, row) => {
+    return sum + parseFloat(row.totalPoints);
+  }, 0);
 
-// Calculate total points across all rows
-const totalPoints = pointData.rows.reduce((sum, row) => {
-  return sum + parseFloat(row.totalPoints);
-}, 0);
+  const usersHasPoints = rows.filter(row => parseFloat(row.totalPoints) > 1);
 
-const myPoints = 113187;
+  console.log(
+    `总积分: ${totalPoints.toLocaleString()}, 用户数: ${
+      rows.length
+    }, 有积分的用户数: ${usersHasPoints.length}`
+  );
 
-const percentage = myPoints / totalPoints;
-console.log(`占比: ${percentage.toFixed(8)}`);
+  return {
+    totalPoints,
+    users: rows.length,
+    usersHasPoints: usersHasPoints.length,
+  };
+}
 
-const tvl = 1000000000;
+const { totalPoints, users, usersHasPoints } = await getPoints();
+const myPoints = 0;
 
-console.log(`Total points across all users: ${totalPoints.toLocaleString()}`);
+const base_mm = 10 ** 8;
+const fdvs = [base_mm, base_mm * 2, base_mm * 3, base_mm * 4, base_mm * 5];
+
+const infos = fdvs.map(d => {
+  const onePointValue = (d * 0.2) / totalPoints;
+  const point = 0.15;
+
+  const info = {
+    fdv: d,
+    onePointValue,
+    uApr: point * onePointValue * 365 * 100,
+  };
+
+  console.log(
+    `fdv 为 ${d / base_mm}mm: 积分价值为 ${onePointValue.toFixed(
+      4
+    )}, 1u 给 ${point} 的场景下年化收益为 ${info.uApr.toFixed(
+      4
+    )}%, 空投价值为 ${myPoints * onePointValue}`
+  );
+
+  return info;
+});
